e92e511aba0588ce268269ce2e6d6a612421ef9e
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-12-18 16:17:22 +0300

    2. Spring Data [#296073]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -1,24 +1,27 @@
 package ru.job4j.accident.control;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accident.service.AccidentService;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.repository.AccidentRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexControl {
+    private final AccidentRepository accidents;
 
-    private AccidentService service;
-
-    @Autowired
-    public IndexControl(AccidentService service) {
-        this.service = service;
+    public IndexControl(AccidentRepository accidents) {
+        this.accidents = accidents;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", service.findAllAccidents());
+        List<Accident> res = new ArrayList<>();
+        accidents.findAll().forEach(res::add);
+        model.addAttribute("accidents", res);
         return "index";
     }
 }
