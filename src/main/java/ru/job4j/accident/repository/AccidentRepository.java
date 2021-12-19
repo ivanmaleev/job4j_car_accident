6acb8c8055dd85892a515f9ea2d1b9830b279172
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-12-19 13:49:49 +0300

    1. Spring ORM [#2093 #241466]

--- src/main/java/ru/job4j/accident/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accident/repository/AccidentRepository.java
@@ -1,20 +1,12 @@
 package ru.job4j.accident.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import ru.job4j.accident.model.Accident;
 
 import java.util.Collection;
-import java.util.Optional;
 
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
     @Override
     Collection<Accident> findAll();
 
-//    @Query("from Accident ac "
-//            + "join fetch ac.type "
-//            + "join fetch ac.rules "
-//            + "where ac.id = : id")
-//    Optional<Accident> findById(@Param("id") int id);
 }
