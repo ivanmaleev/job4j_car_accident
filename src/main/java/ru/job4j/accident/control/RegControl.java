46b1b7cc7ad969dfe170aac2906eb772112dc5f5
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-12-22 19:15:25 +0300

    2. Регистрация пользователя [#296069 #242615]

--- src/main/java/ru/job4j/accident/control/RegControl.java
+++ src/main/java/ru/job4j/accident/control/RegControl.java
@@ -2,12 +2,15 @@ package ru.job4j.accident.control;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.accident.model.User;
 import ru.job4j.accident.repository.AuthorityRepository;
 import ru.job4j.accident.repository.UserRepository;
+
+import java.util.List;
 
 @Controller
 public class RegControl {
@@ -16,14 +19,20 @@ public class RegControl {
     private final UserRepository users;
     private final AuthorityRepository authorities;
 
-    public RegControl(PasswordEncoder encoder, UserRepository users, AuthorityRepository authorities) {
+    public RegControl(PasswordEncoder encoder, UserRepository users,
+                      AuthorityRepository authorities) {
         this.encoder = encoder;
         this.users = users;
         this.authorities = authorities;
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, Model model) {
+        List<User> byUsername = users.findByUsername(user.getUsername());
+        if (byUsername.size() > 0) {
+            model.addAttribute("errorMessage", "Username is busy");
+            return "reg";
+        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
--- src/main/java/ru/job4j/accident/repository/UserRepository.java
+++ src/main/java/ru/job4j/accident/repository/UserRepository.java
@@ -3,5 +3,8 @@ package ru.job4j.accident.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accident.model.User;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    List<User> findByUsername(String username);
 }
